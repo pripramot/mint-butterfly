a8942554d85b5d9b99c241d5adb2837b42d1a109
pripramot <phongwut.w@gmail.com>
2024-11-20 05:30:25 +0700

    Update home.data.tsx

--- src/shared/data/pages/home.data.tsx
+++ src/shared/data/pages/home.data.tsx
@@ -621,7 +621,7 @@ export const contactHome: ContactProps = {
     },
     {
       title: 'Contact',
-      description: ['Mobile: +1 (123) 456-7890', 'Mail: tailnext@gmail.com'],
+      description: ['Mobile: +66 89-1736517', 'Mail: bangkokcustom@hotmail.com'],
       icon: IconPhoneCall,
     },
     {
